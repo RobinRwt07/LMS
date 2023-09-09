229e61fd27661b697e457cf18e1a97e924d713af
Robin Singh <95758949+RobinRwt07@users.noreply.github.com>
2023-09-09 20:49:45 +0530

    Update connect_database.php

--- connect_database.php
+++ connect_database.php
@@ -3,7 +3,7 @@
 $sever="localhost";
 $user="root";
 $database="LMS_Project";
-$connection=mysqli_connect($sever,$user,"Robin@123",$database);
+$connection=mysqli_connect($sever,$user,"*******",$database);
 if(mysqli_connect_errno())
 {
     die("Failed to connect Database. Error: ".mysqli_connect_error());
